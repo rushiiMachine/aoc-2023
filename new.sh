649f1a15f3119a74035638d4dd94449dbda1233b
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-11-20 12:33:33 -0800

    feat: init template

---
+++ new.sh
@@ -0,0 +1,23 @@
+cp -R template day$1
+sed -i "s/\$DAY/day$1/" day$1/Cargo.toml
+sed -i -z "s/\n]/\n    \"day$1\",\n]/" Cargo.toml
+
+echo "<component name=\"ProjectRunConfigurationManager\">
+  <configuration default=\"false\" name=\"Run day$1\" type=\"CargoCommandRunConfiguration\" factoryName=\"Cargo Command\">
+    <option name=\"command\" value=\"run --package day$1 --bin day$1\" />
+    <option name=\"workingDirectory\" value=\"file://\$PROJECT_DIR$/day$1\" />
+    <option name=\"emulateTerminal\" value=\"true\" />
+    <option name=\"channel\" value=\"DEFAULT\" />
+    <option name=\"requiredFeatures\" value=\"true\" />
+    <option name=\"allFeatures\" value=\"false\" />
+    <option name=\"withSudo\" value=\"false\" />
+    <option name=\"buildTarget\" value=\"REMOTE\" />
+    <option name=\"backtrace\" value=\"SHORT\" />
+    <envs />
+    <option name=\"isRedirectInput\" value=\"false\" />
+    <option name=\"redirectInputPath\" value=\"\" />
+    <method v=\"2\">
+      <option name=\"CARGO.BUILD_TASK_PROVIDER\" enabled=\"true\" />
+    </method>
+  </configuration>
+</component>" > .idea/runConfigurations/Run_day$1.xml
