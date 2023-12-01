57741d8ba4804dc48766a235c472075b1450c29d
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2023-12-01 12:13:08 -0800

    refactor: condense day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,13 @@ use std::collections::HashMap;
 use regex::Regex;
 
 fn part1() {
-    let input = include_str!("./input.txt");
-    let mut total = 0u32;
+    let input = include_str!("./part1.txt");
+    let mut total = 0;
 
     for line in input.lines() {
-        let first = line.chars().into_iter().find(|char| char.is_ascii_digit()).unwrap();
-        let last = line.chars().into_iter().rfind(|char| char.is_ascii_digit()).unwrap();
+        let mut digits = line.chars().filter(char::is_ascii_digit);
+        let first = digits.next().unwrap();
+        let last = digits.clone().last().unwrap_or_else(|| first);
 
         total += format!("{first}{last}").parse::<u32>().unwrap();
     }
@@ -17,13 +18,10 @@ fn part1() {
 }
 
 fn part2() {
-    let input = include_str!("./input.txt");
-    let mut total = 0u32;
-
-    let regex = Regex::new(r#"\d|one|two|three|four|five|six|seven|eight|nine"#).unwrap();
-
+    let input = include_str!("./part2.txt");
+    let regex = Regex::new(r#"(?<digit>\d)|(?<phrase>one|two|three|four|five|six|seven|eight|nine)"#).unwrap();
     let phrases_values = HashMap::from([
-        ("one", 1),
+        ("one", 1u8),
         ("two", 2),
         ("three", 3),
         ("four", 4),
@@ -34,24 +32,22 @@ fn part2() {
         ("nine", 9),
     ]);
 
+    let mut total = 0u32;
     for line in input.lines() {
-        let mut results = Vec::new();
-        for (full_match, []) in regex.captures_iter(line).map(|c| c.extract()) {
-            results.push(full_match);
-        }
+        let mut values = regex
+            .captures_iter(line)
+            .filter_map(|capture| {
+                if let Some(digit) = capture.name("digit") {
+                    Some(digit.as_str().parse::<u8>().unwrap())
+                } else if let Some(phrase) = capture.name("phrase") {
+                    Some(*phrases_values.get(phrase.as_str()).unwrap())
+                } else {
+                    None
+                }
+            });
 
-        let first = if results.first().unwrap().chars().into_iter().next().unwrap().is_ascii_digit() {
-            results.first().unwrap().to_string()
-        } else {
-            phrases_values.get(results.first().unwrap()).unwrap().to_string()
-        };
-        let last = if results.last().unwrap().chars().into_iter().next().unwrap().is_ascii_digit() {
-            results.last().unwrap().to_string()
-        } else {
-            phrases_values.get(results.last().unwrap()).unwrap().to_string()
-        };
-
-        total += format!("{first}{last}").parse::<u32>().unwrap();
+        total += format!("{}{}", values.next().unwrap(), values.last().unwrap())
+            .parse::<u32>().unwrap();
     }
 
     println!("{}", total);
